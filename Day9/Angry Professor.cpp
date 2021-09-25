17f21c26873fbd0573a61bf49a258bcecfbda8b6
KUNAL GOSWAMI <76223437+kunal158@users.noreply.github.com>
2021-09-25 14:11:45 +0530

    Create Angry Professor.cpp

---
+++ Day9/Angry Professor.cpp
@@ -0,0 +1,10 @@
+string angryProfessor(int k, vector<int> a) {
+    int count = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] <= 0) {
+                count++;
+            }
+        }
+        return ((count >= k) ? "NO" : "YES");
+
+}
